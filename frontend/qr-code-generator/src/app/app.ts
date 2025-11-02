ffe86aea3fae0be4d560bd4a7bfd43411dea3582
BinuMDX <dissanayakedmbm.20@itfac.mrt.ac.lk>
2025-11-02 10:55:03 +0530

    fix: properly add frontend files

---
+++ frontend/qr-code-generator/src/app/app.ts
@@ -0,0 +1,32 @@
+import { Component, signal } from '@angular/core';
+import { QrService } from './services/qr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.scss'
+})
+export class App {
+  inputText = '';
+  qrImage: string | null = null;
+
+  constructor(private qrService: QrService) {}
+
+  generateQR() {
+    if (!this.inputText.trim()) {
+      alert('Please enter text or URL!');
+      return;
+    }
+
+    this.qrService.generateQR(this.inputText).subscribe({
+      next: (res) => {
+        this.qrImage = res.image;
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Error generating QR code');
+      }
+    });
+  }
+}
---
+++ frontend/qr-code-generator/src/app/services/qr.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QrService {
+
+  private apiUrl = 'http://localhost:3000/api/qr/generate';
+
+  constructor(private http: HttpClient) { }
+
+  generateQR(data: string): Observable<any> {
+    return this.http.post(this.apiUrl, { data });
+  }
+}
